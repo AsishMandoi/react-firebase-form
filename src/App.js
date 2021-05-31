c83f7be46a02c0a4b8c2d9dd4a7f17adef8dd412
AsishMandoi <luckyasish13@gmail.com>
2021-05-31 22:33:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+class UserDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      mob: '',
+    };
+  }
+
+  changeHandler = (event) => {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    let _name = event.target.name;
+    let val = event.target.value;
+    this.setState({[_name]: val});
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    db.collection('users').add({
+      name: this.state.name,
+      email: this.state.email,
+      mob: this.state.mob,
+    }).then(() => {
+      console.log("User details submitted successfully.");
+    });
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.submitHandler}>
+      <h1>Hello</h1>
+      <p>Enter your name:</p>
+      <input type='text' name='name' onChange={this.changeHandler} />
+      <br /><br />
+      <p>Enter your email id:</p>
+      <input type='email' name='email' onChange={this.changeHandler} />
+      <br /><br />
+      <p>Enter your mobile number:</p>
+      <input type='text' name='mob' onChange={this.changeHandler} />
+      <br /><br />
+      <input type='submit' />
+      </form>
+    );
+  }
 }
 
-export default App;
+export default UserDetails;
