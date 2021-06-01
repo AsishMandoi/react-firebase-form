8c4a9889790859ec15935589f8ae8e76fb5e70f9
AsishMandoi <luckyasish13@gmail.com>
2021-06-01 10:17:12 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React from 'react';
-import firebase from 'firebase';
-
-var firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
+import db from './firebasedb';
 
 class UserDetails extends React.Component {
   constructor(props) {
@@ -25,11 +12,7 @@ class UserDetails extends React.Component {
   }
 
   changeHandler = (event) => {
-    console.log(event.target.name);
-    console.log(event.target.value);
-    let _name = event.target.name;
-    let val = event.target.value;
-    this.setState({[_name]: val});
+    this.setState({[event.target.name]: event.target.value});
   }
 
   submitHandler = (event) => {
@@ -38,29 +21,31 @@ class UserDetails extends React.Component {
       name: this.state.name,
       email: this.state.email,
       mob: this.state.mob,
-    }).then(() => {
+    })
+    .then(() => {
       console.log("User details submitted successfully.");
     });
+    console.log(this.state.name, this.state.email, this.state.mob);
     this.setState({
       name: '',
       email: '',
-      message: '',
+      mob: '',
     })
   }
 
   render() {
     return (
       <form onSubmit={this.submitHandler}>
-      <h1>Hello</h1>
+      <h1>Hello </h1>
       <p>Enter your name:</p>
-      <input type='text' name='name' onChange={this.changeHandler} />
+      <input type='text' name='name' onChange={this.changeHandler} value={this.state.name}/>
       <br /><br />
       <p>Enter your email id:</p>
-      <input type='email' name='email' onChange={this.changeHandler} />
+      <input type='email' name='email' onChange={this.changeHandler} value={this.state.email}/>
       <br /><br />
       <p>Enter your mobile number:</p>
-      <input type='text' name='mob' onChange={this.changeHandler} />
-      <br /><br />
+      <input type='text' name='mob' onChange={this.changeHandler} value={this.state.mob}/>
+      <br /><br /><br />
       <input type='submit' />
       </form>
     );
---
+++ src/firebasedb.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = firebase.firestore();
+
+export default db;
